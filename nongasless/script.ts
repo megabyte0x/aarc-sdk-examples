080bd79ea2f87fca6319ba8064d00d18b78e8c00
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2023-11-04 16:18:55 +0530

    permit2 tokens

--- nongasless/script.ts
+++ nongasless/script.ts
@@ -21,7 +21,12 @@ export const tokensTransfer = async () => {
 
   await aarcSDK.executeMigration({
     scwAddress:"0x786E6045eacb96cAe0259cd761e151b68B85bdA7",
-    tokenAndAmount:[{tokenAddress:"0xba62bcfcaafc6622853cca2be6ac7d845bc0f2dc",amount:"10000000000000"}]
+    tokenAndAmount:[
+      {tokenAddress:"0xba62bcfcaafc6622853cca2be6ac7d845bc0f2dc",amount:"10000000000000"},
+      {tokenAddress:"0x022e292b44b5a146f2e8ee36ff44d3dd863c915c",amount:"2000000000000000000"},
+      {tokenAddress:"0xaff4481d10270f50f203e0763e2597776068cbc5",amount:"3000000000000000000"},
+      {tokenAddress:"0xc6fde3fd2cc2b173aec24cc3f267cb3cd78a26b7",amount:"5000000000000"},
+    ]
   })
 }
 
