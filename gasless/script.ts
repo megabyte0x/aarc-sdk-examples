aa1e8a6a9b5444cb26b78f4d4a9806e6d1472be9
Anshul Yadav <55681256+anshulforyou@users.noreply.github.com>
2023-11-20 19:36:23 +0530

    new sdk integration

--- gasless/script.ts
+++ gasless/script.ts
@@ -22,11 +22,11 @@ export const gaslessTokensTransfer = async () => {
   await aarcSDK.executeMigrationGasless({
     senderSigner: signer,
     receiverAddress:"0x786E6045eacb96cAe0259cd761e151b68B85bdA7",
-    tokenAndAmount:[
+    transferTokenDetails:[
       {tokenAddress:"0xbb8dB535d685F2742D6e84EC391c63e6a1Ce3593",amount:BigNumber.from("100000000")},
-      {tokenAddress:"0x022e292b44b5a146f2e8ee36ff44d3dd863c915c",amount:BigNumber.from("20000000000000000")},
+      {tokenAddress:"0x2055b06db421f17c19c655fd4a1c325e8514af67",amount:BigNumber.from("20000000000000000")},
       {tokenAddress:"0xf4ca1a280ebccdaebf80e3c128e55de01fabd893",amount:BigNumber.from("500000000")},
-      {tokenAddress:"0xaff4481d10270f50f203e0763e2597776068cbc5",amount:BigNumber.from("30000000000000000")},
+      {tokenAddress:"0x932ca55b9ef0b3094e8fa82435b3b4c50d713043",tokenIds:["28507"]},
     ],
     gelatoApiKey: GELATO_API_KEY,
   })
@@ -52,7 +52,7 @@ export const gaslessSingleTokenTransfer = async () => {
   await aarcSDK.executeMigrationGasless({
     senderSigner: signer,
     receiverAddress:"0x786E6045eacb96cAe0259cd761e151b68B85bdA7",
-    tokenAndAmount:[
+    transferTokenDetails:[
       {tokenAddress:"0xaff4481d10270f50f203e0763e2597776068cbc5",amount:BigNumber.from("300000000000000")},
     ],
     gelatoApiKey: GELATO_API_KEY,
--- index.ts
+++ index.ts
@@ -1,11 +1,12 @@
 import { gaslessTokensTransfer, gaslessSingleTokenTransfer } from "./gasless/script";
-import { allTokensTransfer, tokensTransfer, nativeTokenTransfer } from "./nongasless/script";
+import { allTokensTransfer, tokensTransfer, nativeTokenTransfer, nftTokenTransfer } from "./nongasless/script";
 import { biconomyWallet } from "./nongasless/biconomy";
 
 import * as dotenv from 'dotenv';
 dotenv.config();
 
 async function main() {
+  // await nftTokenTransfer();
   // await tokensTransfer();
  await gaslessTokensTransfer();
   // await allTokensTransfer();
