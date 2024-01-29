45bc83b759a3277519cd4acea22a6e5f6abbe294
megabyte0x <yashygkf@gmail.com>
2024-01-29 19:55:05 +0530

    safe smart wallet tutorial ✅

--- src/smart-accounts/safe_SW.ts
+++ src/smart-accounts/safe_SW.ts
@@ -1,28 +1,28 @@
 import { ethers, BigNumber } from "ethers";
 import { Migrator, WALLET_TYPE } from "@aarc-xyz/migrator";
-import { MUMBAI_RPC_URL, PRIVATE_KEY, API_KEY, ChainID } from "../constants";
+import { SEPOLIA_RPC_URL, PRIVATE_KEY, API_KEY, ChainID } from "../constants";
 
 
-let provider = new ethers.providers.JsonRpcProvider(MUMBAI_RPC_URL);
+let provider = new ethers.providers.JsonRpcProvider(SEPOLIA_RPC_URL);
 let signer = new ethers.Wallet(PRIVATE_KEY, provider);
 let eoaAddress = signer.address;
 
 let aarcSDK = new Migrator({
-    rpcUrl: MUMBAI_RPC_URL,
-    chainId: ChainID.MUMBAI,
+    rpcUrl: SEPOLIA_RPC_URL,
+    chainId: ChainID.SEPOLIA,
     apiKey: API_KEY,
 });
 
 /**
- * Function to fetch the Biconomy Smart Wallets associated with the `eoaAddress`, and  deploy and transfer native tokens to the Biconomy Smart Wallet.
+ * Function to fetch the Safe Smart Wallets associated with the `eoaAddress`, and  deploy and transfer native tokens to the Safe Smart Wallet.
  */
 
-async function BiconomySW() {
+async function SafeSW() {
 
     try {
         // get all the smart wallets associated with the eoaAddress
         const smartWalletAddresses = await aarcSDK.getSmartWalletAddresses(
-            WALLET_TYPE.BICONOMY,
+            WALLET_TYPE.SAFE,
             eoaAddress
         );
         console.log(' smartWalletAddresses ', smartWalletAddresses);
@@ -32,19 +32,20 @@ async function BiconomySW() {
     }
 
     try {
-        const reponse = await aarcSDK.transferNativeAndDeploy({
-            walletType: WALLET_TYPE.BICONOMY,
+        // deploy and transfer native tokens to the Safe Smart Wallet
+        const response = await aarcSDK.transferNativeAndDeploy({
+            walletType: WALLET_TYPE.SAFE,
             owner: eoaAddress,
-            receiver: "0xcba2d1f7522757f4f1b2e1f7ce54612c61fbd366", //Change the receiver address to the SA address
+            receiver: "0x1a6e27BE6d0902eBd2D6FE533790c6A06A892F63", //Change the receiver address to the SA address
             signer: signer,
             deploymentWalletIndex: 0, // Change the index to the index of the SA
             amount: BigNumber.from("10000")._hex // Change the string to the amount you want to transfer
         });
-        console.log("Transfer and Deploy Response: ", reponse);
+        console.log("Transfer and Deploy Response: ", response);
     } catch (error) {
         console.error(' error ', error);
     }
 }
 
-// to run this function: `npm run biconomySW`
-BiconomySW();
+// to run this function: `npm run safeSW`
+SafeSW();
